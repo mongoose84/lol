3dcca64dc9e3f8bcad6398c92915a703074ca000
Jeppe <jeppe.kronborg@pm.me>
2025-11-03 12:45:35 +0100

    Created folders and files to start connecting to the database

---
+++ server/RiotProxy/Domain/Entities/Match.cs
@@ -0,0 +1,8 @@
+namespace RiotProxy.Domain
+{
+    public class LolMatch
+    {
+        public string MatchId { get; set; } = string.Empty;
+        public string Puuid { get; set; } = string.Empty;
+    }
+}
---
+++ server/RiotProxy/Domain/Entities/Person.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace RiotProxy.Domain
+{
+    public class Person
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+    }
+}
